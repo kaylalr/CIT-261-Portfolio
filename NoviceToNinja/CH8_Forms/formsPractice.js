bd2bb6a94e7c812f0a73fa05c16c8f693565ea65
Kayla Roberts <klr3of8@gmail.com>
2018-05-12 16:45:33 -0600

    Started Chapter 8

---
+++ NoviceToNinja/CH8_Forms/formsPractice.js
@@ -0,0 +1,51 @@
+// This is getting the first form.
+//const form = document.forms[0];
+// That is the same as doing it this way:
+//const form = document.getElementsByTagname('form)[0];
+// You can also do it this way.  But if the forms have the same names, it causes problems.
+//const form = document.form.search;
+const form = document.forms['search'];
+form.addEventListener ('submit', search, false);
+
+function search (event) {
+    alert(`You Searched for: ${input.value}`);
+    // This stops the form from being submitted
+    event.preventDefault();
+}
+
+//const [input,button] = form.elements;
+// Same thing as above
+const input = form.searchInput;
+input.value = 'Search Here';
+input.addEventListener('focus', function(){
+    if (input.value === 'Search Here') {
+        input.value = '';
+    }
+}, false);
+
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    }
+},false);
+
+input.addEventListener('change', () => console.log('changed'),false);
+
+
+
+
+// HERO PRACTICE
+
+const formHero = document.forms['hero'];
+formHero.addEventListener('submit', makeHero, false)
+
+function makeHero(event) {
+    event.preventDefault(); //prevent form from being submitted
+    const hero = {}; //create empty object
+    hero.name = formHero.heroName.value; //create name property based on the input field's vlaue
+    alert(JSON.stringify(hero)); //convert object to JSON string and display in alert dialog
+    console.log(hero.name);
+    hero.realName = formHero.realName.value;
+    console.log(JSON.stringify((hero.realName)));
+    return hero;
+}
